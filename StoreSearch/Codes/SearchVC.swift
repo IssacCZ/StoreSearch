f176e14269f8f736ffd4b35ac3dfb2cd7ccf85c6
IssacCZ <sheldowkay@icloud.com>
2015-12-31 19:39:19 +0800

    Creating the UI

--- StoreSearch/Codes/SearchVC.swift
+++ StoreSearch/Codes/SearchVC.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
 }
 
