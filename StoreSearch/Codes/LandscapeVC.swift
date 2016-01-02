b71f4b4fd9544a0e1d9219543d969d78b1c348a3
IssacCZ <sheldowkay@icloud.com>
2016-01-03 01:34:28 +0800

    Adding the scroll view

--- StoreSearch/Codes/LandscapeVC.swift
+++ StoreSearch/Codes/LandscapeVC.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class LandscapeVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
+    }
+    
     deinit {
         print("deinit \(self)")
     }
