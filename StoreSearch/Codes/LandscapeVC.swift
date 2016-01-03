b0e76800cc6ad7d52d353f892490faa69a3a5481
IssacCZ <sheldowkay@icloud.com>
2016-01-03 11:41:10 +0800

    Nothing found

--- StoreSearch/Codes/LandscapeVC.swift
+++ StoreSearch/Codes/LandscapeVC.swift
@@ -49,14 +49,31 @@ class LandscapeVC: UIViewController {
             firstTime = false
             
             switch search.state {
-            case .NotSearchedYet, .NoResults:
+            case .NotSearchedYet:
                 break
+            case .NoResults:
+                showNothingFoundLabel()
             case .Loading:
                 showSpinner()
             case .Results(let list):
                 titleButton(list)
             }
         }
+    }
+    
+    private func showNothingFoundLabel() {
+        let label = UILabel(frame: CGRect.zero)
+        label.text = "Nothing Found"
+        label.textColor = UIColor.whiteColor()
+        label.backgroundColor = UIColor.clearColor()
+        label.sizeToFit()
+        
+        var rect = label.frame
+        rect.size.width = ceil(rect.size.width / 2) * 2
+        rect.size.height = ceil(rect.size.height / 2) * 2
+        label.frame = rect
+        label.center = CGPoint(x: CGRectGetMidX(scrollView.bounds), y: CGRectGetMidY(scrollView.bounds))
+        view.addSubview(label)
     }
     
     private func showSpinner() {
@@ -71,8 +88,10 @@ class LandscapeVC: UIViewController {
         hideSpinner()
         
         switch search.state {
-        case .Loading, .NoResults, .NotSearchedYet:
+        case .Loading, .NotSearchedYet:
             break
+        case .NoResults:
+            showNothingFoundLabel()
         case .Results(let list):
             titleButton(list)
         }
